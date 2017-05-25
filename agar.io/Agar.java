58b35584a71f617ab0cd6b5df6deac747e5a3978
turnerb1 <brturner8@gmail.com>
2017-05-25 11:12:52 -0700

    Revert "other comments"
    This reverts commit 7021289a24834b58269da05dce8d9ffd251bb688.

---
+++ agar.io/Agar.java
@@ -0,0 +1,47 @@
+import greenfoot.*;
+
+public class Agar  extends ScrollWorld{
+    private Counter theCounter; //mass counter
+    
+    public Agar() {
+        //Creating a world size of 700x500 cells with 1x1 pixels
+        super(900, 600, 1, 4900, 4600);
+        
+        int startX = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
+        int startY = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
+        
+        addCameraFollower(new Cell(), 0, 0);
+        setCameraLocation(startX, startY);
+        
+        theCounter = new Counter();
+        addObject(theCounter, getWidth()/2, getHeight()/2);
+        
+        spawnProteins();
+        spawnViruses();
+    }
+    
+    public Counter getCounter() {
+        return theCounter;
+    }
+    
+    public void spawnPlayer() {
+        addObject(new Cell(),(int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2)), (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2)));
+    }
+    
+    public void spawnProteins() {
+        //World world = getWorld();
+        for (int i = 0; i < 1000; i++) {
+            int x = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
+            int y = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
+            addObject(new Protein((int)(Math.random() * 5)), x, y);
+        }
+    }
+    
+    public void spawnViruses(){
+        for(int i = 0; i < 50; i++){
+            int x = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
+            int y = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
+            addObject(new Virus(), x, y);
+        }
+    }
+}
