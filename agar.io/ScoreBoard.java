623e3596b7aaaa916f9c1a7827a6143ca646ceb7
CeciliaMartinez <martinez.roots@gmail.com>
2017-05-25 10:16:00 -0700

    scoreboard

--- agar.io/ScoreBoard.java
+++ agar.io/ScoreBoard.java
@@ -14,14 +14,39 @@ public class ScoreBoard extends Actor {
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     private static final String prefix = "Scores: ";
+<<<<<<< HEAD
+    //String players = agar.players();
+    agar agarWorld = (agar)getWorld();
+    List<Cell> playersList = agarWorld.getObjects(Cell.class);
+    public Cell players(){
+        //agar agarWorld = (agar)getWorld();
+        //List<Cell> playersList = agarWorld.getObjects(Cell.class);
+        Cell player = null;
+        for(int i = 0; i < playersList.size(); i++){
+            //player = playersList.get(i);
+        }
+        
+        for (Object obj : agarWorld.getObjects(Cell.class)){
+            player = (Cell) obj; // sub-casting
+            // change location of 'ps'
+        }
+        
+        return player;
+    }
+=======
     //String players = agar.players();    
+>>>>>>> 64e03f7135bc8d9bc2bd888aba5f07b75544d23d
 
     public void act() 
     {
-        //Add your action code here.
+        //updateImage(players().getMass());
         
     }    
+<<<<<<< HEAD
+
+=======
     
+>>>>>>> 64e03f7135bc8d9bc2bd888aba5f07b75544d23d
     public void updateImage(int massText){
         setImage(new GreenfootImage(prefix + " " + Integer.toString(massText), 18, Color.black, new Color(0, 0, 0, 0)));
     }
