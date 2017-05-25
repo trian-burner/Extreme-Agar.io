db621cfffee5e760f2e986403eeca2dc97f761d1
turnerb1 <brturner8@gmail.com>
2017-05-25 10:32:43 -0700

    added documentation to Agar class

--- agar.io/agar.java
+++ agar.io/agar.java
@@ -5,31 +5,36 @@ public class Agar  extends ScrollWorld{
     
     public Agar() {
         //Creating a world size of 700x500 cells with 1x1 pixels
-        super(900, 600, 1, 4900, 4600);
+        super(900, 600, 1, 4900, 4600); // creates the new world and scroll world
         
+        // returns a random int within the playable region of the world for the cell to spawn at
         int startX = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
         int startY = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
         
+        // creates a new cell and sets the camera's position to be centered over the cell
         addCameraFollower(new Cell(), 0, 0);
         setCameraLocation(startX, startY);
         
+        // a reference for the counter that the cell can use
         theCounter = new Counter();
         addObject(theCounter, getWidth()/2, getHeight()/2);
         
+        // initially spawns the proteins and viruses into the world
         spawnProteins();
         spawnViruses();
     }
     
     public Counter getCounter() {
-        return theCounter;
+        return theCounter; // returns the counter for the cell
     }
     
     public void spawnPlayer() {
+        // this will add a new player at this location
         addObject(new Cell(),(int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2)), (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2)));
     }
     
     public void spawnProteins() {
-        //World world = getWorld();
+        // spawns a given number of proteins into the world at random locations within the playable region of the field
         for (int i = 0; i < 1000; i++) {
             int x = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
             int y = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
@@ -38,6 +43,7 @@ public class Agar  extends ScrollWorld{
     }
     
     public void spawnViruses(){
+        // spawns a given number of viruses into the world at random locations within the playable region of the field
         for(int i = 0; i < 50; i++){
             int x = (int)(Math.random() * (getFullWidth() - getWidth()) + (getWidth() / 2));
             int y = (int)(Math.random() * (getFullHeight() - getHeight()) + (getHeight() / 2));
