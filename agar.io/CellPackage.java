d92efdd8bf1429d520ecc542c396cee95d635948
trian-burner <trian.burner@outlook.com>
2017-06-12 10:02:05 -0700

    fixes

--- agar.io/CellPackage.java
+++ agar.io/CellPackage.java
@@ -13,51 +13,7 @@ public class CellPackage implements java.io.Serializable {
     int rotation;
     int speed;
     Color color = Color.white;
-<<<<<<< HEAD
-        
-    /**
-     * Creates a new empty CellPackage
-    */
-    public CellPackage() {}
-  
-        /**
-         * Creates a new CellPackage and fills all of the variables with the given Cell's values
-         * 
-         * @param cell The cell that the package is being created to "hold"
-         */
-        public CellPackage(Cell cell) {
-            this.name = cell.name;
-            this.size = cell.size;
-            this.mass = cell.mass;
-            this.x = cell.getGlobalX();
-            this.y = cell.getGlobalY();
-            this.rotation = cell.getRotation();
-            this.speed = cell.speed;
-            this.color = cell.color;
-        }
-        
-        /**
-         * Updates the package with the current act's values
-         * 
-         * @param cell The cell that the package will be updated with
-         */
-        public void update(Cell cell) {
-            this.name = name;
-            this.size = cell.size;
-            this.mass = cell.mass;
-            this.x = cell.getGlobalX();
-            this.y = cell.getGlobalY();
-            this.rotation = cell.getRotation();
-            this.speed = cell.speed;
-            this.color = cell.color;
-        }
 
-        public String toString() {
-            return "Name: " + name + "; Size: " + size + "; Mass: " + mass + "; X: " + x + "; Y: " + y + "; R: " + rotation + "; Speed: " + speed;
-        }
-
-=======
-    
     /**
      * Creates a new empty CellPackage
      */
@@ -85,7 +41,7 @@ public class CellPackage implements java.io.Serializable {
      * @param cell The cell that the package will be updated with
      */
     public void update(Cell cell) {
-        this.name = name;
+        this.name = cell.name;
         this.size = cell.size;
         this.mass = cell.mass;
         this.x = cell.getGlobalX();
@@ -94,5 +50,4 @@ public class CellPackage implements java.io.Serializable {
         this.speed = cell.speed;
         this.color = cell.color;
     }
->>>>>>> 3a1c6d9227b4795a2ab86517f768c961c01b1534
 }
